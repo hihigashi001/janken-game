49cde6e52d35f2faea4b51859c748b742fd0c837
hihigashi001 <hihigashi001@gmail.com>
2023-07-27 15:13:43 +0900

    カードにした

--- src/components/shared/Wrapper.tsx
+++ src/components/shared/Wrapper.tsx
@@ -2,8 +2,8 @@ import { WrapperProps } from "@/types";
 
 export const Wrapper = (props: WrapperProps) => {
   return (
-    <div className="w-full h-full p-4">
-      <div className="flex flex-col gap-8 justify-center w-full h-full p-8 border border-gray-400 rounded-md">
+    <div className="w-full h-full p-4 flex justify-center">
+      <div className="flex flex-col gap-4 justify-center w-full max-w-md py-8 px-6 bg-white border border-gray-200 rounded-lg shadow-md">
         {props.children}
       </div>
     </div>
